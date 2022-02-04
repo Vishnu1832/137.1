8de2264060a8ddf09437365a5353723ee4b09ff5
Vishnu1832 <84667502+Vishnu1832@users.noreply.github.com>
2022-02-04 19:56:38 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,65 @@
+video = "";
+status = "";
+objects = [];
+
+function preload()
+{
+
+}
+
+function setup() 
+{
+  canvas = createCanvas(400,300);
+  canvas.center();
+  video = createCapture(VIDEO);
+  video.size(380,380);
+  video.hide();
+  objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+  document.getElementById("status").innerHTML = "Status : Detecting Objects";
+}
+
+function draw()
+{
+    image(video, 0, 0, 400,300);
+    if (status != "")
+    {
+        objectDetector.detect(video, gotResult);
+        for (i = 0; i < objects.length; i++)
+        {
+            document.getElementById("status").innerHTML = "Status : Obejct Detected";
+            document.getElementById("number_of_objects").innerHTML = "Number of objects Detected are : " +objects.length;
+
+            fill("#FF0000");
+            percent = floor(objects[i].confidence * 100);
+            text(objects[i].label + "" + percent + "%", objects[i].x + 15, objects[i].y + 15);
+            noFill();
+            stroke("#FF0000");
+            rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+        }
+    }
+}
+
+function start()
+{
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status: Detecting Objects"
+}
+
+function modelLoaded()
+{
+    console.log("Model Loaded !")
+    status = true;
+    video.loop();
+    video.speed(1);
+    video.volume(0);
+}
+
+function gotResult(error, results)
+{
+    if(error)
+    {
+        console.log(error);
+    }
+    console.log(results);
+    objects = results;
+}
